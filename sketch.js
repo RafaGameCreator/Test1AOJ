d5293f01656acd3881ed130e96e1231d1f2fd648
RafaGameCreator <107970102+RafaGameCreator@users.noreply.github.com>
2023-12-13 18:39:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,153 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var afonsoOJegue, aojIMG;
+var afonsesGroup;
+var life1, life2, life3;
+var l1, l2, l3;
+var lifeT = 3;
+var gameState;
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+  aojIMG = loadImage("assets/zombie.png")
+  bgImg = loadImage("assets/bg.jpeg")
+  l1 = loadImage("assets/heart_1.png");
+  l2 = loadImage("assets/heart_2.png");
+  l3 = loadImage("assets/heart_3.png");
+}
+
+function setup() {
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adicionando a imagem de fundo
+bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 900
+  
+
+//criando o sprite do jogador
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = false
+   player.setCollider("rectangle",0,0,300,300)
+
+   afonsesGroup = new Group()
+
+   life1 = createSprite(displayWidth - 150, 40, 20, 20)
+   life1.visible = false
+   life1.addImage("l1", l1)
+   life1.scale = 0.5
+
+   life2 = createSprite(displayWidth - 150, 40, 20, 20)
+   life2.visible = false
+   life2.addImage("l2", l2)
+   life2.scale = 0.5
+
+   life3 = createSprite(displayWidth - 150, 40, 20, 20)
+   life3.visible = true
+   life3.addImage("l3", l3)
+   life3.scale = 0.
+}
+
+function draw() {
+  background(0); 
+  if(lifeT = 0){
+    gameState = "lost"
+  }
+  if(afonsesGroup.isTouching(player)){
+    for(i = 0; i < afonsesGroup.length; i = i + 1){
+    //i++ tbm serve então pod usar que nem desodorante
+    if(afonsesGroup[i].isTouching(player)){
+      afonsesGroup[i].destroy()
+      lifeT - 1
+    }
+    }
+    lifeT - 1
+  }
+
+  if(lifeT === 3){
+    life1.visible = false
+    life2.visible = false
+    life3.visible = true
+  }
+
+  if(lifeT === 2){
+    life1.visible = false
+    life2.visible = true
+    life3.visible = false
+  }
+
+  if(lifeT === 1){
+    life1.visible = true
+    life2.visible = false
+    life3.visible = false
+  }
+
+  
+  if(lifeT === 1){
+    if(keyDown("UP_ARROW")||touches.length>0){
+      player.y = player.y-7
+    }
+    if(keyDown("DOWN_ARROW")||touches.length>0){
+     player.y = player.y+7
+    }
+    
+    if(keyDown("LEFT_ARROW")||touches.length>0){
+      player.x = player.x-7
+    }
+    if(keyDown("RIGHT_ARROW")||touches.length>0){
+     player.x = player.x+7
+    }
+  }
+
+
+  //movendo o jogador para cima e para baixo e tornando o jogo compatível com dispositivos móveis usando toques
+  if(lifeT > 1){
+    if(keyDown("UP_ARROW")||touches.length>0){
+      player.y = player.y-30
+    }
+    if(keyDown("DOWN_ARROW")||touches.length>0){
+     player.y = player.y+30
+    }
+    
+    if(keyDown("LEFT_ARROW")||touches.length>0){
+      player.x = player.x-30
+    }
+    if(keyDown("RIGHT_ARROW")||touches.length>0){
+     player.x = player.x+30
+    }
+  }
+
+
+//solte balas e mude a imagem do atirador para a posição de tiro quando a tecla de espaço for pressionada
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//o jogador volta à imagem original quando pararmos de pressionar a barra de espaço
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+}
+enimyAfonsito();
+drawSprites();
+}
+
+
+function enimyAfonsito(){
+  if(frameCount%50===0){
+    afonsoOJegue = createSprite(random(500, 1100), random(500, 1100), 50, 50);
+    afonsoOJegue.addImage("aojIMG", aojIMG)
+    afonsoOJegue.scale = 0.3
+    afonsoOJegue.debug = false
+    afonsoOJegue.setCollider("rectangle",0,0,300,300)
+    afonsoOJegue.lifetime = 400
+    afonsoOJegue.velocityX = -10
+    afonsesGroup.add(afonsoOJegue)
+    }
+  }
